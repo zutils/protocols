f18c387ac7ca63caf6e3c818c6d710103f63221d
James Prince <34577138+zutils@users.noreply.github.com>
2018-12-31 11:11:11 -0500

    Sync + Send stuff
    Allows for compatability with multithreaded lazy_static

--- src/propagator.rs
+++ src/propagator.rs
@@ -3,16 +3,18 @@ use crate::{Transport, Destination, TransportToModuleGlue, ModuleToTransportGlue
 use crate::common::CommonModule;
 
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex, RwLock};
 
 #[derive(Default)]
 /// If we want to be able to have multiple modules per plugin, we use this.
+/// Note that each module requires a lock to use it. The propogation itself should only be locked when adding modules.
 pub struct TransportNode {
-    nodes: Vec<Box<Propagator>>,
-    modules: HashMap<String, Box<TransportToModuleGlue + Sync>>, // String is schema_url of some type... using Schema gave me errors.
+    nodes: Vec<Arc<RwLock<Propagator + Send + Sync>>>,
+    modules: HashMap<String, Arc<Mutex<TransportToModuleGlue + Send>>>, // String is schema_url of some type... using Schema gave me errors.
 }
 
 impl TransportNode {
-    pub fn add_interface<M: 'static + TransportToModuleGlue + CommonModule + Sync>(&mut self, module: M) {
+    pub fn add_interface<M: 'static + TransportToModuleGlue + CommonModule + Send>(&mut self, module: M) {
         use crate::utils::AsStringer;
         match module.get_info(&Destination::new()) {
             Ok(ref vec_info) if vec_info.get_vec().len() > 1 => println!("Module is returning too much info!"),
@@ -20,24 +22,33 @@ impl TransportNode {
                 // If we have exactly one module_info, then we need to add that module's schema as a key
                 let info = &vec_info.get_vec().to_vec()[0];
                 let schema = info.get_schema();
-                self.modules.insert(schema.as_string().to_string(), Box::new(module)); },
+                self.modules.insert(schema.as_string().to_string(), Arc::new(Mutex::new(module))); },
             Ok(ref vec_info) if vec_info.get_vec().len() == 0 => println!("No Info available from module!"),
             Ok(_) => println!("Cannot add module to Transport Node!"),
             Err(e) => println!("Cannot add module to Transport Node! {:?}", e),
         }
     }
 
-    pub fn add_node<T: 'static + Propagator>(&mut self, node: T) {
-        self.nodes.push(Box::new(node));
+    pub fn add_node<T: 'static + Propagator + Send + Sync>(&mut self, node: T) {
+        self.nodes.push(Arc::new(RwLock::new(node)));
     }
 
     pub fn handle_appropriate_modules(&self, transport: &Transport) -> Vec<Transport> {
         use crate::utils::AsStringer;
 
-        let (transports, errors): (Vec<_>, Vec<_>) = self.modules.iter()
+        //println!("Attempting to handle {:?}", transport);
+
+        let appropriate_modules = self.modules.iter()
+            //.inspect(|(schema, _)| println!("Checking for appropriate schema: {:?}", schema) )
             .filter(|(schema, _)| !transport.has_destination() || *schema == transport.get_destination().as_string())
-            .map(|(_, module)| module)
-            .map(|module| module.handle_transport(transport) )
+            .map(|(_, module)| module).collect::<Vec<_>>();
+
+        if appropriate_modules.len() > 0 {
+            println!("Found {} appropriate modules.", appropriate_modules.len());
+        }
+
+        let (transports, errors): (Vec<_>, Vec<_>) = appropriate_modules.iter()
+            .map(|module| module.lock().unwrap().handle_transport(transport) )
             .partition(Result::is_ok);
 
         let mut transports: Vec<_> = transports.into_iter().map(Result::unwrap).flatten().collect();
@@ -54,7 +65,7 @@ impl TransportNode {
 }
 
 // Allow being able to call module functions on node
-impl ModuleToTransportGlue for TransportNode {}
+impl<'a> ModuleToTransportGlue for TransportNode {}
 
 /// For now, broadcast to ALL transport. We never know if there are duplicate plugins for the same schema
 pub trait Propagator {
@@ -68,7 +79,7 @@ impl Propagator for TransportNode {
         
         // Propagate and collect.
         let mut new_transports: Vec<Transport> = self.nodes.iter()
-            .map(|node| node.propagate_transport(transport) )
+            .map(|node| node.read().unwrap().propagate_transport(transport) )
             .flatten()
             .collect();
         ret.append(&mut new_transports);
