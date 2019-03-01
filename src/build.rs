5728f05ae93903ee7b478c20a3a7dd322c477d79
James Prince <34577138+zutils@users.noreply.github.com>
2019-02-28 20:39:03 -0500

    New autogenerate code for transport

--- src/build.rs
+++ src/build.rs
@@ -1,12 +1,18 @@
 pub mod buildfunctions;
-pub mod transport_autogen;
 pub mod logging;
 
 use failure::Error;
 use std::path::PathBuf;
+use std::io::Write;
+
+fn autogenerate_rpc<W: Write + ?Sized>(rpc: &pb_rs::types::RpcService, w: &mut W) -> Result<(), pb_rs::errors::Error> {
+
+	Ok(())
+}
 
 fn create_protobuf(proto_path: &PathBuf) -> Result<(), Error> {
-	let _generated_rs_file = buildfunctions::build_rust_code_from_protobuffer(proto_path)?;
+	let _generated_rs_file = buildfunctions::build_rust_code_from_protobuffer_with_options(proto_path, Vec::new(),
+		Box::new(|rpc, writer| autogenerate_rpc(rpc, writer)))?;
 
 	let hash = buildfunctions::add_file_to_ipfs(proto_path)?;
 	let _schema_url_path = buildfunctions::write_schema_url(proto_path, &hash)?;
