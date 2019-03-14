88b3b8661cc101c85bb91e196e3019ec502df3be
James Prince <34577138+zutils@users.noreply.github.com>
2019-03-14 17:46:12 -0400

    Added new From trait

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,13 @@
 use crate::autogen::transport::{ SchemaIdentifier};
 
-impl<'a> From<&str> for SchemaIdentifier {
+impl From<&str> for SchemaIdentifier {
     fn from(f: &str) -> SchemaIdentifier {
         SchemaIdentifier::new(f.to_string())
+    }
+}
+
+impl From<String> for SchemaIdentifier {
+    fn from(f: String) -> SchemaIdentifier {
+        SchemaIdentifier::new(f)
     }
 }
